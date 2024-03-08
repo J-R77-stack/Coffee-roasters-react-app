27e19c38dba6f3afaf0246c80863863a10cc6ab7
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-03-08 16:23:47 +0000

    add twtter and instgram icons to footer

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,17 +1,24 @@
 import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Footer() {
   return (
     <footer>
-      <Link to="/">coffeeroasters</Link>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/about">About us</NavLink>
       <NavLink to="/plan">Create Your Plan</NavLink>
       <Link to="http://facebook.com">
         <FontAwesomeIcon icon={faFacebookSquare} />
+      </Link>
+      <Link to="http://twitter.com">
+        <FontAwesomeIcon icon={faTwitter} />
+      </Link>
+      <Link to="http://instagram.com">
+        <FontAwesomeIcon icon={faInstagram} />
       </Link>
     </footer>
   );

7aef34c330dee54930a1b261011ac1bd73738f0e
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-03-09 08:42:14 +0000

    add error element to handle incorrect url requestes using useRoute error, add useNavigate to allow user to return to previous page

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Cart from "./features/cart/Cart";
 import Plan from "./features/plan/Plan";
 import About from "./features/about/About";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -16,9 +18,10 @@ const router = createBrowserRouter([
       {
         path: "/about",
         element: <About />,
+        errorElement: <Error />,
       },
-      { path: "/cart", element: <Cart /> },
-      { path: "/plan", element: <Plan /> },
+      { path: "/cart", element: <Cart />, errorElement: <Error /> },
+      { path: "/plan", element: <Plan />, errorElement: <Error /> },
     ],
   },
 ]);

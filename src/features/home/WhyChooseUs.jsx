9d7aedb89d8b6a76898150c3825119c877e95ed8
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-03-11 17:10:10 +0000

    create why choose us section

--- src/features/home/WhyChooseUs.jsx
+++ src/features/home/WhyChooseUs.jsx
@@ -7,6 +7,30 @@ function WhyChooseUs() {
         featured in our range. This means working closely with the best coffee
         growers to give you a more impactful experience on every level.
       </p>
+      <div className="bg-dark-cyan text-center">
+        <img src="/src/images/desktop/icon-coffee-bean.svg" alt="coffee bean" />
+        <h2>Best Quality</h2>
+        <p>
+          Discover an endless variety of the world’s best artisan coffee from
+          each of our roasters.
+        </p>
+      </div>
+      <div className="bg-dark-cyan text-center">
+        <img src="/src/images/desktop/icon-gift.svg" alt="gift" />
+        <h2>Exclusive benefits</h2>
+        <p>
+          Special offers and swag when you subscribe, including 30% off your
+          first shipment.
+        </p>
+      </div>
+      <div className="bg-dark-cyan text-center">
+        <img src="/src/images/desktop/icon-truck.svg" alt="truck" />
+        <h2>Free shipping</h2>
+        <p>
+          We cover the cost and coffee is delivered fast. Peak freshness:
+          guaranteed.
+        </p>
+      </div>
     </div>
   );
 }

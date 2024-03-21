a9041193de2ec783e94bfea049efecfda2501202
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-03-21 18:00:59 +0000

    style how it works section for sm above screens

--- src/features/home/HowItWorks.jsx
+++ src/features/home/HowItWorks.jsx
@@ -1,47 +1,49 @@
 function HowItWorks() {
   return (
     <div className="mt-6">
-      <h3 className="pt-10 text-center text-h4 font-bold text-grey-grey">
+      <h3 className="pt-10 text-center text-h4 font-bold text-grey-grey sm:text-left">
         How it works
       </h3>
-      <div>
-        <h1 className=" mt-10 text-center text-h1 font-bold text-pale-orange">
-          01
-        </h1>
-        <h4 className="mb-6 text-center text-h4 font-bold leading-h4">
-          Pick your coffee
-        </h4>
-        <p className="text-center font-barlow">
-          Select from our evolving range of artisan coffees. Our beans are
-          ethically sourced and we pay fair prices for them. There are new
-          coffees in all profiles every month for you to try out.
-        </p>
-      </div>
-      <div>
-        <h1 className=" mt-10 text-center text-h1 font-bold text-pale-orange">
-          02
-        </h1>
-        <h4 className="mb-6 text-center text-h4 font-bold leading-h4">
-          Choose the frequency
-        </h4>
-        <p className="text-center font-barlow">
-          Customize your order frequency, quantity, even your roast style and
-          grind type. Pause, skip or cancel your subscription with no commitment
-          through our online portal.
-        </p>
-      </div>
-      <div>
-        <h1 className=" mt-10 text-center text-h1 font-bold text-pale-orange">
-          03
-        </h1>
-        <h4 className="mb-6 text-center text-h4 font-bold leading-h4">
-          Receive and enjoy!
-        </h4>
-        <p className="pb-8 text-center font-barlow">
-          We ship your package within 48 hours, freshly roasted. Sit back and
-          enjoy award-winning world-class coffees curated to provide a distinct
-          tasting experience.
-        </p>
+      <div className="sm:grid sm:grid-cols-3 sm:gap-5 ">
+        <div>
+          <h1 className=" mt-10 text-center text-h1 font-bold text-pale-orange sm:text-left">
+            01
+          </h1>
+          <h4 className="mb-6 text-center text-h4 font-bold leading-h4 sm:pr-6 sm:text-left">
+            Pick your coffee
+          </h4>
+          <p className="text-center font-barlow sm:text-left">
+            Select from our evolving range of artisan coffees. Our beans are
+            ethically sourced and we pay fair prices for them. There are new
+            coffees in all profiles every month for you to try out.
+          </p>
+        </div>
+        <div>
+          <h1 className=" mt-10 text-center text-h1 font-bold text-pale-orange sm:text-left">
+            02
+          </h1>
+          <h4 className="mb-6 text-center  text-h4 font-bold leading-h4 sm:text-left">
+            Choose the frequency
+          </h4>
+          <p className="text-center font-barlow sm:text-left">
+            Customize your order frequency, quantity, even your roast style and
+            grind type. Pause, skip or cancel your subscription with no
+            commitment through our online portal.
+          </p>
+        </div>
+        <div>
+          <h1 className=" mt-10 text-center text-h1 font-bold text-pale-orange sm:text-left">
+            03
+          </h1>
+          <h4 className="mb-6 text-center text-h4 font-bold leading-h4 sm:text-left">
+            Receive and enjoy!
+          </h4>
+          <p className="pb-8 text-center font-barlow sm:text-left">
+            We ship your package within 48 hours, freshly roasted. Sit back and
+            enjoy award-winning world-class coffees curated to provide a
+            distinct tasting experience.
+          </p>
+        </div>
       </div>
       <button className="mx-16 my-8 h-16 w-52 rounded-xl bg-dark-cyan font-bold text-white hover:bg-teal-300">
         Create your plan

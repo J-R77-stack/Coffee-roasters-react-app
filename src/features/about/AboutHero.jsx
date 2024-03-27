0d8aac1089a9278cdc41e174499be44a4ad282ef
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-03-27 17:48:09 +0000

    start work on About us section creating abouthero jsx and styling for mobile screens

---
+++ src/features/about/AboutHero.jsx
@@ -0,0 +1,17 @@
+function AboutHero() {
+  return (
+    <div className="grid grid-rows-2 rounded-xl bg-[url('/src/images/mobile/image-hero-whitecup.jpg')] bg-cover bg-center bg-no-repeat p-4 pb-16 pt-12 text-center">
+      <h1 className="mt-20 text-h3 font-bold leading-h3 text-light-cream">
+        About us
+      </h1>
+      <p className="font-barlow text-body leading-body text-light-cream">
+        Coffeeroasters began its journey of exotic discovery in 1999,
+        highlighting stories of coffee from around the world. We have since been
+        dedicated to bring the perfect cup - from bean to brew - in every
+        shipment.
+      </p>
+    </div>
+  );
+}
+
+export default AboutHero;
--- src/features/about/about.jsx
+++ src/features/about/about.jsx
@@ -1,13 +1,9 @@
+import AboutHero from "./AboutHero";
+
 function About() {
   return (
     <div>
-      <h1>About us</h1>
-      <p>
-        Coffeeroasters began its journey of exotic discovery in 1999,
-        highlighting stories of coffee from around the world. We have since been
-        dedicated to bring the perfect cup - from bean to brew - in every
-        shipment.
-      </p>
+      <AboutHero />
     </div>
   );
 }

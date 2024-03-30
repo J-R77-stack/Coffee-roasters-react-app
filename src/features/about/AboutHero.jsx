3d51982763c011127eeb89f371f2e523bab348ec
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-03-30 11:02:15 +0000

    finish adding styles to abouthero mobile view

--- src/features/about/AboutHero.jsx
+++ src/features/about/AboutHero.jsx
@@ -17,7 +17,7 @@ function AboutHero() {
         <h3 className="mt-20 text-center text-h3 font-bold leading-h3 text-black">
           Our commitment
         </h3>
-        <p className="mt-10 text-center font-barlow text-body leading-body text-grey-grey">
+        <p className="mt-10 pb-32 text-center font-barlow text-body leading-body text-grey-grey">
           We’re built on a simple mission and a commitment to doing good along
           the way. We want to make it easy for you to discover and brew the
           world’s best coffee at home. It all starts at the source. To locate
@@ -31,12 +31,12 @@ function AboutHero() {
           distinctive to their native growing region.
         </p>
       </div>
-      <div className=" mt-10 h-96 rounded-xl bg-[url('/src/images/mobile/bg-quality.png')] bg-cover bg-center bg-no-repeat">
-        <div className="  h-32 w-64  rounded-xl bg-[url('/src/images/mobile/image-quality.jpg')] bg-cover bg-center bg-no-repeat"></div>
-        <h3 className="mt-20 text-center text-h3 font-bold leading-h3 text-black">
+      <div className="grid h-[600px] place-content-center rounded-xl bg-[url('/src/images/mobile/bg-quality.png')] bg-cover bg-center bg-no-repeat">
+        <div className="mb-10 ml-10 h-[150px] w-64 rounded-xl bg-[url('/src/images/mobile/image-quality.jpg')] bg-cover bg-center bg-no-repeat"></div>
+        <h3 className=" text-center text-h3 font-bold leading-h3 text-light-cream">
           Uncompromising quality
         </h3>
-        <p className="mt-10 text-center font-barlow text-body leading-body text-grey-grey">
+        <p className="mt-8 pl-5 pr-5 text-center font-barlow text-body leading-body text-grey-grey">
           Although we work with growers who pay close attention to all stages of
           harvest and processing, we employ, on our end, a rigorous quality
           control program to avoid over-roasting or baking the coffee dry. Every
@@ -44,6 +44,52 @@ function AboutHero() {
           is to roast consistent, user-friendly coffee, so that brewing is easy
           and enjoyable.
         </p>
+      </div>
+      <div>
+        <h4 className="mb-12 mt-32 text-center text-h4 font-bold leading-h4 text-grey-grey">
+          Our Headquarters
+        </h4>
+      </div>
+      <div className="grid grid-rows-3">
+        <div>
+          <img
+            src="/src/images/desktop/svg/illustration-uk.svg"
+            alt=""
+            className="m-auto"
+          />
+          <h3 className="mb-5 mt-10 text-center text-h3 font-bold leading-h3">
+            United Kingdom
+          </h3>
+          <p className="mb-16 pl-24 pr-24 text-center font-barlow text-body leading-body text-grey-grey">
+            68 Asfordby Rd Alcaston SY6 1YA +44 1241 918425
+          </p>
+        </div>
+        <div>
+          <img
+            src="/src/images/desktop/svg/illustration-canada.svg"
+            alt=""
+            className="m-auto"
+          />
+          <h3 className="mb-5 mt-10 text-center text-h3 font-bold leading-h3">
+            Canada
+          </h3>
+          <p className="mb-16 pl-20 pr-20 text-center font-barlow text-body leading-body text-grey-grey">
+            1528 Eglinton Avenue Toronto Ontario M4P 1A6 +1 416 485 2997
+          </p>
+        </div>
+        <div>
+          <img
+            src="/src/images/desktop/svg/illustration-australia.svg"
+            alt=""
+            className="m-auto"
+          />
+          <h3 className="mb-5 mt-10 text-center text-h3 font-bold leading-h3">
+            Australia
+          </h3>
+          <p className="pl-20 pr-20 text-center font-barlow text-body leading-body text-grey-grey">
+            36 Swanston Street Kewell Victoria <br /> +61 4 9928 3629
+          </p>
+        </div>
       </div>
     </>
   );

631d45b32fa78cbefc4fdd0b8a0d249f6917a4f2
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-04-02 17:14:44 +0100

    style about page for sm and lg screens, style plan page for sm screens, style footer for lg screens

--- src/features/about/AboutHero.jsx
+++ src/features/about/AboutHero.jsx
@@ -1,11 +1,11 @@
 function AboutHero() {
   return (
     <>
-      <div className="grid grid-rows-2 rounded-xl  bg-[url('/src/images/mobile/image-hero-whitecup.jpg')] bg-cover bg-center bg-no-repeat p-4 pb-16 pt-12 text-center sm:bg-[url('/src/images/tablet/image-hero-whitecup.jpg')] sm:p-8 sm:text-left">
+      <div className="grid grid-rows-2 rounded-xl  bg-[url('/src/images/mobile/image-hero-whitecup.jpg')] bg-cover bg-center bg-no-repeat p-4 pb-16 pt-12 text-center sm:bg-[url('/src/images/tablet/image-hero-whitecup.jpg')] sm:p-8 sm:text-left lg:bg-[url('/src/images/desktop/image-hero-whitecup.jpg')] lg:p-16">
         <h3 className="mt-20 text-h3 font-bold leading-h3 text-light-cream sm:mb-8">
           About us
         </h3>
-        <p className="font-barlow text-body leading-body text-light-cream  sm:pr-40">
+        <p className="font-barlow text-body leading-body text-light-cream sm:pr-40 lg:pr-[350px]">
           Coffeeroasters began its journey of exotic discovery in 1999,
           highlighting stories of coffee from around the world. We have since
           been dedicated to bring the perfect cup - from bean to brew - in every
@@ -13,9 +13,9 @@ function AboutHero() {
         </p>
       </div>
       <div className="sm:grid sm:grid-cols-2">
-        <div className=" mt-32 h-96 rounded-xl bg-[url('/src/images/mobile/image-commitment.jpg')] bg-cover bg-center bg-no-repeat sm:h-[560px] sm:bg-[url('/src/images/tablet/image-commitment.jpg')]"></div>
+        <div className=" mt-32 h-96 rounded-xl bg-[url('/src/images/mobile/image-commitment.jpg')] bg-cover bg-center bg-no-repeat sm:h-[560px] sm:bg-[url('/src/images/tablet/image-commitment.jpg')] lg:sm:bg-[url('/src/images/desktop/image-commitment.jpg')] lg:h-[500px] lg:w-[400px]"></div>
         <div className="sm:ml-10">
-          <h3 className="mt-20 text-center text-h3 font-bold leading-h3 text-black sm:mt-36 sm:text-left">
+          <h3 className="mt-20 text-center text-h3 font-bold leading-h3 text-black sm:mt-36 sm:text-left lg:mt-48">
             Our commitment
           </h3>
           <p className="mt-10 pb-32 text-center font-barlow text-body leading-body text-grey-grey sm:mt-4 sm:text-left">
@@ -33,36 +33,38 @@ function AboutHero() {
           </p>
         </div>
       </div>
-      <div className="grid h-[600px] place-content-center rounded-xl bg-[url('/src/images/mobile/bg-quality.png')] bg-cover bg-center bg-no-repeat sm:mt-[80px] sm:h-[660px] sm:bg-[url('/src/images/tablet/bg-quality.png')]">
-        <div className="mb-10 ml-10 h-[150px] w-64 rounded-xl bg-[url('/src/images/mobile/image-quality.jpg')] bg-cover bg-center bg-no-repeat  sm:h-[300px] sm:w-[600px] sm:bg-[url('/src/images/tablet/image-quality.jpg')]"></div>
-        <h3 className=" text-center text-h3 font-bold leading-h3 text-light-cream">
-          Uncompromising quality
-        </h3>
-        <p className="mt-8 pl-5 pr-5 text-center font-barlow text-body leading-body text-grey-grey">
-          Although we work with growers who pay close attention to all stages of
-          harvest and processing, we employ, on our end, a rigorous quality
-          control program to avoid over-roasting or baking the coffee dry. Every
-          bag of coffee is tagged with a roast date and batch number. Our goal
-          is to roast consistent, user-friendly coffee, so that brewing is easy
-          and enjoyable.
-        </p>
+      <div className=" grid h-[600px] place-content-center rounded-xl bg-[url('/src/images/mobile/bg-quality.png')] bg-cover bg-center bg-no-repeat sm:mt-[80px] sm:h-[660px] sm:bg-[url('/src/images/tablet/bg-quality.png')] lg:flex lg:h-[450px] lg:flex-row">
+        <div className="lg:bg-[url('/src/images/desktop/image-quality.jpg')]lg:p-0 mb-10 ml-10 h-[150px] w-64 rounded-xl bg-[url('/src/images/mobile/image-quality.jpg')] bg-cover bg-center  bg-no-repeat sm:h-[300px] sm:w-[600px] sm:bg-[url('/src/images/tablet/image-quality.jpg')] lg:order-last lg:m-0  lg:mr-8 lg:h-[500px] lg:basis-1/2 lg:p-8"></div>
+        <div className="lg:mt-10 lg:basis-1/2 lg:p-8">
+          <h3 className=" text-center text-h3 font-bold leading-h3 text-light-cream">
+            Uncompromising quality
+          </h3>
+          <p className="mt-8 pl-5 pr-5 text-center font-barlow text-body leading-body text-grey-grey">
+            Although we work with growers who pay close attention to all stages
+            of harvest and processing, we employ, on our end, a rigorous quality
+            control program to avoid over-roasting or baking the coffee dry.
+            Every bag of coffee is tagged with a roast date and batch number.
+            Our goal is to roast consistent, user-friendly coffee, so that
+            brewing is easy and enjoyable.
+          </p>
+        </div>
       </div>
       <div>
-        <h4 className="mb-12 mt-32 text-center text-h4 font-bold leading-h4 text-grey-grey">
+        <h4 className="mb-12 mt-32 text-center text-h4 font-bold leading-h4 text-grey-grey lg:ml-10 lg:text-left">
           Our Headquarters
         </h4>
       </div>
-      <div className="grid grid-rows-3">
+      <div className="grid grid-rows-3 lg:m-10 lg:flex">
         <div>
           <img
             src="/src/images/desktop/svg/illustration-uk.svg"
             alt=""
-            className="m-auto"
+            className="m-auto lg:m-0"
           />
-          <h3 className="mb-5 mt-10 text-center text-h3 font-bold leading-h3">
+          <h3 className="mb-5 mt-10 text-center text-h3 font-bold leading-h3 lg:text-left">
             United Kingdom
           </h3>
-          <p className="mb-16 pl-24 pr-24 text-center font-barlow text-body leading-body text-grey-grey">
+          <p className="mb-16 pl-24 pr-24 text-center font-barlow text-body leading-body text-grey-grey lg:pl-0 lg:pr-52 lg:text-left">
             68 Asfordby Rd Alcaston SY6 1YA +44 1241 918425
           </p>
         </div>
@@ -70,12 +72,12 @@ function AboutHero() {
           <img
             src="/src/images/desktop/svg/illustration-canada.svg"
             alt=""
-            className="m-auto"
+            className="m-auto lg:m-0"
           />
-          <h3 className="mb-5 mt-10 text-center text-h3 font-bold leading-h3">
+          <h3 className="mb-5 mt-10 text-center text-h3 font-bold leading-h3 lg:text-left">
             Canada
           </h3>
-          <p className="mb-16 pl-20 pr-20 text-center font-barlow text-body leading-body text-grey-grey">
+          <p className="mb-16 pl-20 pr-20 text-center font-barlow text-body leading-body text-grey-grey lg:pl-0 lg:pr-48 lg:text-left">
             1528 Eglinton Avenue Toronto Ontario M4P 1A6 +1 416 485 2997
           </p>
         </div>
@@ -83,12 +85,12 @@ function AboutHero() {
           <img
             src="/src/images/desktop/svg/illustration-australia.svg"
             alt=""
-            className="m-auto"
+            className="m-auto lg:m-0"
           />
-          <h3 className="mb-5 mt-10 text-center text-h3 font-bold leading-h3">
+          <h3 className="mb-5 mt-10 text-center text-h3 font-bold leading-h3 lg:text-left">
             Australia
           </h3>
-          <p className="pl-20 pr-20 text-center font-barlow text-body leading-body text-grey-grey">
+          <p className="pl-20 pr-20 text-center font-barlow text-body leading-body text-grey-grey lg:pl-0  lg:text-left">
             36 Swanston Street Kewell Victoria <br /> +61 4 9928 3629
           </p>
         </div>
--- src/features/plan/Plan.jsx
+++ src/features/plan/Plan.jsx
@@ -3,52 +3,70 @@ import Questions from "./Questions";
 function Plan() {
   return (
     <>
-      <div className="grid grid-rows-2 rounded-xl bg-[url('/src/images/mobile/image-hero-blackcup.jpg')] bg-cover bg-center bg-no-repeat  pb-16 pt-10 text-center">
+      <div className="grid grid-rows-2 rounded-xl bg-[url('/src/images/mobile/image-hero-blackcup.jpg')] bg-cover bg-center bg-no-repeat pb-16 pt-10 text-center sm:bg-[url('/src/images/tablet/image-hero-blackcup.jpg')] sm:pl-6 sm:text-left">
         <h2 className="mt-20 pl-3 pr-3 text-h2 font-bold leading-h2 text-light-cream">
           Create a plan
         </h2>
-        <p className="mt-4 pb-10 pl-4 pr-4 font-barlow text-body leading-body text-grey-grey">
+        <p className="mt-4 pb-10 pl-4 pr-4 font-barlow text-body leading-body text-grey-grey sm:pr-52">
           Build a subscription plan that best fits your needs. We offer an
           assortment of the best artisan coffees from around the globe delivered
           fresh to your door.
         </p>
       </div>
-      <div className="mt-16 h-[1100px] rounded-xl bg-[url('/src/images/mobile/bg-steps.png')] bg-cover bg-center bg-no-repeat">
-        <div className="grid grid-rows-3 text-center">
-          <div>
-            <h1 className="leading-h1 mt-10 text-h1 font-bold text-pale-orange">
+      <div className="mt-16 h-[1100px] rounded-xl bg-[url('/src/images/mobile/bg-steps.png')] bg-cover bg-center bg-no-repeat sm:h-[610px] sm:bg-[url('/src/images/tablet/bg-steps.png')]">
+        <div className="hidden sm:flex sm:pl-8 sm:pt-20 lg:pl-[2rem] lg:pt-10">
+          <div className="sm:flex">
+            <div className="rounded-full border-2 border-solid border-dark-cyan sm:h-11 sm:w-11 "></div>
+            <div>
+              <hr className="border-2 border-solid border-pale-orange sm:mt-5 sm:w-40 lg:w-[15rem]" />
+            </div>
+          </div>
+          <div className="sm:flex">
+            <div className="rounded-full border-2 border-solid border-dark-cyan sm:h-11 sm:w-11"></div>
+            <div>
+              <hr className="border-2 border-solid border-pale-orange sm:mt-5 sm:w-[155px] lg:w-[15rem]" />
+            </div>
+          </div>
+          <div className="sm:flex">
+            <div className="rounded-full border-2 border-solid border-dark-cyan sm:h-11 sm:w-11"></div>
+          </div>
+        </div>
+
+        <div className="grid grid-rows-3 text-center sm:flex sm:p-8 sm:text-left">
+          <div className="sm:basis-1/3">
+            <h1 className="leading-h1 mt-10 text-h1 font-bold text-pale-orange sm:mt-2">
               01
             </h1>
             <h3 className="mb-5 text-h3 font-bold leading-h3 text-light-cream">
               Pick your coffee
             </h3>
-            <p className="pl-3 pr-3 font-barlow text-body leading-body text-grey-grey">
+            <p className="pl-3 pr-3 font-barlow text-body leading-body text-grey-grey sm:p-0">
               Select from our evolving range of artisan coffees. Our beans are
               ethically sourced and we pay fair prices for them. Theres new
               coffees in all profiles every month for you to try out.
             </p>
           </div>
-          <div>
-            <h1 className="leading-h1 mt-10 text-h1 font-bold text-pale-orange">
+          <div className="sm:basis-1/3">
+            <h1 className="leading-h1 mt-10 text-h1 font-bold text-pale-orange sm:mt-2 sm:pl-2">
               02
             </h1>
-            <h3 className="mb-5 text-h3 font-bold leading-h3 text-light-cream">
+            <h3 className="mb-5 text-h3 font-bold leading-h3 text-light-cream sm:pl-2">
               Choose the frequency
             </h3>
-            <p className="pl-3 pr-3 font-barlow text-body leading-body text-grey-grey">
+            <p className="pl-3 pr-3 font-barlow text-body leading-body text-grey-grey sm:p-0 sm:pl-2">
               Customize your order frequency, quantity, even your roast style
               and grind type. Pause, skip or cancel your subscription with no
               commitment through our online portal.
             </p>
           </div>
-          <div>
-            <h1 className="leading-h1 mt-10 text-h1 font-bold text-pale-orange">
+          <div className="sm:basis-1/3">
+            <h1 className="leading-h1 mt-10 text-h1 font-bold text-pale-orange sm:mt-2">
               03
             </h1>
             <h3 className="mb-5 text-h3 font-bold leading-h3 text-light-cream">
               Recieve and enjoy!
             </h3>
-            <p className="pl-3 pr-3 font-barlow text-body leading-body text-grey-grey">
+            <p className="pl-3 pr-3 font-barlow text-body leading-body text-grey-grey sm:p-0">
               We ship your package within 48 hours, freshly roasted. Sit back
               and enjoy award-winning world-class coffees curated to provide a
               distinct tasting experience.

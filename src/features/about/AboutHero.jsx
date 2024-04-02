6c8c1ab0c94ad56a74ed96e68cdce96bcaa38ba7
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-04-02 13:36:16 +0100

    finish styling about page for sm screens

--- src/features/about/AboutHero.jsx
+++ src/features/about/AboutHero.jsx
@@ -1,7 +1,7 @@
 function AboutHero() {
   return (
     <>
-      <div className="grid grid-rows-2 rounded-xl  bg-cover bg-center bg-no-repeat p-4 pb-16 pt-12 text-center sm:bg-[url('/src/images/tablet/image-hero-whitecup.jpg')] sm:p-8 sm:text-left">
+      <div className="grid grid-rows-2 rounded-xl  bg-[url('/src/images/mobile/image-hero-whitecup.jpg')] bg-cover bg-center bg-no-repeat p-4 pb-16 pt-12 text-center sm:bg-[url('/src/images/tablet/image-hero-whitecup.jpg')] sm:p-8 sm:text-left">
         <h3 className="mt-20 text-h3 font-bold leading-h3 text-light-cream sm:mb-8">
           About us
         </h3>
@@ -12,27 +12,29 @@ function AboutHero() {
           shipment.
         </p>
       </div>
-      <div className=" mt-32 h-96 rounded-xl bg-[url('/src/images/mobile/image-commitment.jpg')] bg-cover bg-center bg-no-repeat"></div>
-      <div>
-        <h3 className="mt-20 text-center text-h3 font-bold leading-h3 text-black">
-          Our commitment
-        </h3>
-        <p className="mt-10 pb-32 text-center font-barlow text-body leading-body text-grey-grey">
-          We’re built on a simple mission and a commitment to doing good along
-          the way. We want to make it easy for you to discover and brew the
-          world’s best coffee at home. It all starts at the source. To locate
-          the specific lots we want to purchase, we travel nearly 60 days a year
-          trying to understand the challenges and opportunities in each of these
-          places. We collaborate with exceptional coffee growers and empower a
-          global community of farmers through with well above fair-trade
-          benchmarks. We also offer training, support farm community
-          initiatives, and invest in coffee plant science. Curating only the
-          finest blends, we roast each lot to highlight tasting profiles
-          distinctive to their native growing region.
-        </p>
+      <div className="sm:grid sm:grid-cols-2">
+        <div className=" mt-32 h-96 rounded-xl bg-[url('/src/images/mobile/image-commitment.jpg')] bg-cover bg-center bg-no-repeat sm:h-[560px] sm:bg-[url('/src/images/tablet/image-commitment.jpg')]"></div>
+        <div className="sm:ml-10">
+          <h3 className="mt-20 text-center text-h3 font-bold leading-h3 text-black sm:mt-36 sm:text-left">
+            Our commitment
+          </h3>
+          <p className="mt-10 pb-32 text-center font-barlow text-body leading-body text-grey-grey sm:mt-4 sm:text-left">
+            We’re built on a simple mission and a commitment to doing good along
+            the way. We want to make it easy for you to discover and brew the
+            world’s best coffee at home. It all starts at the source. To locate
+            the specific lots we want to purchase, we travel nearly 60 days a
+            year trying to understand the challenges and opportunities in each
+            of these places. We collaborate with exceptional coffee growers and
+            empower a global community of farmers through with well above
+            fair-trade benchmarks. We also offer training, support farm
+            community initiatives, and invest in coffee plant science. Curating
+            only the finest blends, we roast each lot to highlight tasting
+            profiles distinctive to their native growing region.
+          </p>
+        </div>
       </div>
-      <div className="grid h-[600px] place-content-center rounded-xl bg-[url('/src/images/mobile/bg-quality.png')] bg-cover bg-center bg-no-repeat">
-        <div className="mb-10 ml-10 h-[150px] w-64 rounded-xl bg-[url('/src/images/mobile/image-quality.jpg')] bg-cover bg-center bg-no-repeat"></div>
+      <div className="grid h-[600px] place-content-center rounded-xl bg-[url('/src/images/mobile/bg-quality.png')] bg-cover bg-center bg-no-repeat sm:mt-[80px] sm:h-[660px] sm:bg-[url('/src/images/tablet/bg-quality.png')]">
+        <div className="mb-10 ml-10 h-[150px] w-64 rounded-xl bg-[url('/src/images/mobile/image-quality.jpg')] bg-cover bg-center bg-no-repeat  sm:h-[300px] sm:w-[600px] sm:bg-[url('/src/images/tablet/image-quality.jpg')]"></div>
         <h3 className=" text-center text-h3 font-bold leading-h3 text-light-cream">
           Uncompromising quality
         </h3>

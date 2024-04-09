2352d51376c1edbb7a386338d7d8c2227fa9f848
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-04-09 17:55:06 +0100

    experimenting with accordia component to render order from button clicks

--- src/features/plan/Products.jsx
+++ src/features/plan/Products.jsx
@@ -8,57 +8,49 @@ import { useState } from "react";
 function Products(props) {
   const [isOpen, setIsOpen] = useState(false);
 
-  const [cartItems, setCartItems] = useState({ 1: 0, 2: 0, 3: 0 });
-
-  const addToCart = (id) => {
-    setCartItems((cartItems) => ({
-      ...cartItems,
-      [id]: cartItems[id] + 1,
-    }));
-    console.log(cartItems);
-  };
+  const [count, setCount] = useState("");
 
   return (
-    <>
-      <div className="item relative flex w-[340px] flex-col items-center rounded-xl  sm:w-full">
-        <button
-          className="flex w-full items-center justify-between bg-light-cream py-4  text-left text-h4 font-bold leading-h4 text-grey-grey"
-          onClick={() => setIsOpen((prev) => !prev)}
-        >
-          {props.title1}
+    <div className="item relative flex w-[340px] flex-col items-center rounded-xl  sm:w-full">
+      <button
+        className="flex w-full items-center justify-between bg-light-cream py-4  text-left text-h4 font-bold leading-h4 text-grey-grey"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        {props.title1}
 
-          {!isOpen ? <Arrow /> : <ArrowUp />}
-        </button>
-        {isOpen && (
-          <div className=" grid overflow-hidden transition-all duration-300 ease-in-out sm:grid-cols-3 sm:gap-3">
-            {QUESTION.map((question) => (
-              <div key={question.id}>
-                <button
-                  onClick={() => addToCart(question.id)}
-                  className="rounded-xl bg-zinc-200 p-5 text-left hover:bg-pale-orange focus:bg-dark-cyan sm:h-[240px] "
-                >
-                  <h4 className="pb-2 text-h4 font-bold text-black">
-                    {question.option1}
-                  </h4>
-                  <p className="font-barlow text-body leading-body">
-                    {question.info1}
-                  </p>
-                </button>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-      {QUESTION.map((question) => {
-        if (cartItems[question.id] > 0) {
-          return (
-            <div>
-              <h3>{question.option1}</h3>
+        {!isOpen ? <Arrow /> : <ArrowUp />}
+      </button>
+      {isOpen && (
+        <div className=" grid overflow-hidden transition-all duration-300 ease-in-out sm:grid-cols-3 sm:gap-3">
+          {QUESTION.map((question) => (
+            <div key={question.id}>
+              <button
+                onClick={() => setCount(() => question.option1)}
+                className="rounded-xl bg-zinc-200 p-5 text-left hover:bg-pale-orange focus:bg-dark-cyan sm:h-[240px] "
+              >
+                <h4 className="pb-2 text-h4 font-bold text-black">
+                  {question.option1}
+                </h4>
+                <p className="font-barlow text-body leading-body">
+                  {question.info1}
+                </p>
+              </button>
             </div>
-          );
-        }
-      })}
-    </>
+          ))}
+        </div>
+      )}
+      <div className="grid grid-rows-1 rounded-xl bg-[url('/src/images/mobile/bg-steps.png')] bg-cover bg-center bg-no-repeat p-8 text-left">
+        <h4 className=" mb-4 text-h4 leading-h4 text-grey-grey">
+          ORDER SUMMARY
+        </h4>
+        <h3>
+          “I drink my coffee as {count} , with a _____ type of bean. _____
+          ground ala _____, sent to me _____.” Create my plan! Order Summary “I
+          drink coffee _____, with a _____ type of bean. _____ ground ala _____,
+          sent to me _____.”
+        </h3>
+      </div>
+    </div>
   );
 }
 
